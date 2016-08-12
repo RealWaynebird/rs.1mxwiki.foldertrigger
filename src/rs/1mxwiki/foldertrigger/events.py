5ca24ecf538fb77c14fdbe7e1b841b6382fde81f
RealWaynebird <Ferdinand.Sedlmair@gmx.net>
2016-08-12 10:57:21 +0000

    tried to add triger on page creation for default folder view on page

--- src/rs/1mxwiki/foldertrigger/events.py
+++ src/rs/1mxwiki/foldertrigger/events.py
@@ -1,2 +1,6 @@
 def notifyFolderIsAdded(folder, event):
-    folder.REQUEST.RESPONSE.redirect(folder.absolute_url() + "/++add++Document")  
+    folder.REQUEST.RESPONSE.redirect(folder.absolute_url() + "/++add++Document") 
+
+def notifyDocumentIsAdded(document, event):
+    folder = aq_parent(document)
+    folder.setDefaultPage(document) 
