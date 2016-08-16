0adf6998b9be7cf9dd706abf3cb0ffb5e6fd01b6
RealWaynebird <Ferdinand.Sedlmair@gmx.net>
2016-08-16 08:09:03 +0000

    A new page sets now the default view of the parent folder on itself.

--- src/rs/1mxwiki/foldertrigger/events.py
+++ src/rs/1mxwiki/foldertrigger/events.py
@@ -1,6 +1,9 @@
+from Acquisition import aq_inner, aq_parent
+from plone import api
+
 def notifyFolderIsAdded(folder, event):
     folder.REQUEST.RESPONSE.redirect(folder.absolute_url() + "/++add++Document") 
 
 def notifyDocumentIsAdded(document, event):
-    folder = aq_parent(document)
-    folder.setDefaultPage(document) 
+    folder = aq_parent(aq_inner(document))
+    folder.setDefaultPage(getattr(document,"id"))
